af86d941fb4c0d95c85168c0e71bece43d797420
Hamza Sahapurwala <hamjuzyt@gmail.com>
2025-08-21 09:19:43 +0530

    Javascript: Logs, Number & NaN

---
+++ Javascript/learningbasicjs.js
@@ -0,0 +1,25 @@
+console.log("Hello World"); // ! This is the print statement of Javascript
+
+// * console.log's will go to the next line and then print their statements
+
+// ! Number Data Type
+
+// * The number data type can store all the types of numbers like +,- & floating numbers
+
+// * This data type can store all the things, so there is no other data types like double and float (as they are in C)
+
+console.log(40+15, 2/5, 8*2, 2-5, 4%2, 3 ** 2); // * It prints: 55 0.4 16 -3 0 9
+
+// * PEMDAS - Parenthesis Exponent Multiplication Division Addition Subtraction (Highest Precedence to the Left and Lowest Precedence to the Right)
+
+// * That is the rule followed by Javascript when doing numerical operations
+
+// ! NaN - Not a Number
+
+// * It's the value when concepts like infinity comes into play
+
+// * It is still considered as part of the Number data type
+
+// * Any operations with NaN will return NaN
+
+console.log(0/0, 1 + NaN); // * These operations return the value NaN
